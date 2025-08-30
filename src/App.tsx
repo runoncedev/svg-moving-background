ffa15ba5a5896f66279b21bda6dd689baca10fea
oncet <camilorivera86@gmail.com>
2025-08-30 00:00:32 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,53 @@
 import "./App.css";
 
-const items = 3;
-const columns = 4;
+const items = 4;
+const columns = 5;
 
 function App() {
   return (
     <div style={{ display: "flex" }}>
       <svg style={{ border: "1px solid" }} width="240px" height="90px">
         <rect x="20" y="20" width="200" height="50" fill="pink" />
-
-        {Array.from({ length: items }, (_, i) => (
-          <g key={i} transform={`translate(40) rotate(-0)`}>
-            <rect
-              width="20px"
-              height="120px"
-              fill="transparent"
-              stroke="black"
-            />
-            <circle
-              cx="10"
-              cy="90"
-              r="10"
-              fill="blue"
-              className={`move-${i}`}
-            />
-            <style>
-              {`
-                .move-${i} {
-                  animation: moveup-${i} 6s linear infinite;
-                  animation-delay: ${i * 2}s;
-                }
-
-                @keyframes moveup-${i} {
-                  0% {
-                    transform: translateY(0);
+        <g transform="translate(-40)">
+          {Array.from({ length: columns }).map((_, column) => (
+            <g key={column} transform={`translate(${column * 50})`}>
+              {Array.from({ length: items }, (_, item) => (
+                <g key={column + item} transform="rotate(-45)">
+                  <rect
+                    width="20px"
+                    height="120px"
+                    fill="transparent"
+                    stroke="transparent"
+                  />
+                  <circle
+                    cx="10"
+                    cy="90"
+                    r="10"
+                    fill="blue"
+                    className={`move-${column}${item}`}
+                  />
+                  <style>
+                    {`
+                  .move-${column}${item} {
+                    animation: moveup-${column}${item} 6s linear infinite;
+                    animation-delay: ${item * 2 + column * 1.25}s;
                   }
-                  100% {
-                    transform: translateY(-90px);
+  
+                  @keyframes moveup-${column}${item} {
+                    0% {
+                      transform: translateY(90px);
+                    }
+                    100% {
+                      transform: translateY(-90px);
+                    }
                   }
-                }
-              `}
-            </style>
-          </g>
-        ))}
+                `}
+                  </style>
+                </g>
+              ))}
+            </g>
+          ))}
+        </g>
       </svg>
     </div>
   );
