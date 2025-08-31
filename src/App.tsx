1cd71f1ac770bdcd354aabaf638d1221c4d6259e
oncet <camilorivera86@gmail.com>
2025-08-31 00:09:11 -0300

    rotate

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ const columns = 5;
 
 function App() {
   return (
-    <div style={{ display: "flex" }}>
+    <button
+      style={{
+        display: "flex",
+        padding: "10px 20px",
+        width: "200px",
+        position: "relative",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+        backgroundColor: "white",
+        borderRadius: "10px",
+        border: "4px solid blue",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: "0.1em",
+      }}
+    >
       <svg
         style={{
-          border: "4px solid blue",
+          // border: "4px solid blue",
           borderRadius: "10px",
           cursor: "pointer",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          // display: "none",
         }}
         width="200px"
         height="50px"
@@ -32,23 +54,25 @@ function App() {
                       stroke="transparent"
                     />
                     <path
-                      d="M10,120 L12.5,125 L18,125 L14,128 L15.5,133.5 L10,131 L4.5,133.5 L6,128 L2,125 L7.5,125 Z"
+                      d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"
                       fill="white"
                       className={`move-${key}`}
                     />
                     <style>
                       {`
                   .move-${key} {
+                    transform-box: fill-box;
+                    transform-origin: center;
                     animation: moveup-${key} ${animationDuration}s linear infinite;
                     animation-delay: ${animationDelay}s;
                   }
   
                   @keyframes moveup-${key} {
                     0% {
-                      transform: translateY(130px);
+                      transform: translateY(130px) rotate(0deg);
                     }
                     100% {
-                      transform: translateY(-130px);
+                      transform: translateY(-130px) rotate(360deg);
                     }
                   }
                 `}
@@ -60,7 +84,8 @@ function App() {
           ))}
         </g>
       </svg>
-    </div>
+      <span style={{ zIndex: 1000 }}>Button</span>
+    </button>
   );
 }
 
